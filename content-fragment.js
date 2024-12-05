8f7ae16dcf6582db2b8514a94a4f1eea7587cc8c
Mugdhashrivastava <84615010+Mugdhashrivastava@users.noreply.github.com>
2024-12-05 16:52:03 +0530

    printing data on aem component

--- content-fragment.js
+++ content-fragment.js
@@ -1,1 +1,16 @@
+// dropped a hello world component in aem page (sites)
+// created a js file in client lib (the code here is that file only)
+// what is happening ? 1. used the class of hello world called "cmp-helloworld" to display things 
 
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const helloWorldComponent = document.querySelector(".cmp-helloworld");
+
+ 
+    if (helloWorldComponent) {
+
+        helloWorldComponent.textContent = "Hello from mugdha"; 
+    } else {
+        console.error("Hello World component (.cmp-helloworld) not found!");
+    }
+});
